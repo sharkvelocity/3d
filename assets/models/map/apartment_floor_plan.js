9ed8a36ce1aa1ae495c13cf923274d43a885f780
sharkvelocity <71565691+sharkvelocity@users.noreply.github.com>
2025-08-25 17:45:51 -0700

    Update apartment_floor_plan.js

--- assets/models/map/apartment_floor_plan.js
+++ assets/models/map/apartment_floor_plan.js
@@ -1,25 +1,12 @@
-// assets/models/map/apartment_floor_plan.js
 (function () {
   window.MAP_DEF = {
-    title: "Apartment Floor Plan",
+    title: "Apartment",
     file: "apartment_floor_plan.glb",
-    scale: 1,
+    scale: 1.0,
     rotationY: 0,
-
-    // adjust once you check spawn point inside Blender/Inspector
     offset: { x: 0, y: 0, z: 0 },
-
-    spawn: { x: 0, y: 1.8, z: 0 },
-    vanSpawn: { x: 0, y: 1.8, z: 0 },
-
-    exteriorMode: "exterior",
-    exterior: [
-      { x:-40, z:-40 }, { x:40, z:-40 }, { x:40, z:40 }, { x:-40, z:40 }
-    ],
-
-    rooms: [
-      { name:"Van", poly:[ {x:-5,z:5}, {x:5,z:5}, {x:5,z:12}, {x:-5,z:12} ] }
-      // TODO: break apartment into Living Room / Kitchen / Bedroom once polys are measured
-    ]
+    // Set this to a good front-door / van zone for the apartment file:
+    vanZone: { center: { x: 0.0, y: 1.8, z: 0.0 }, radius: 11 },
+    rooms: []
   };
 })();
