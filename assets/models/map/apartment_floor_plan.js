6d5fadf430b99a29386467c587c588fca91bfe59
sharkvelocity <71565691+sharkvelocity@users.noreply.github.com>
2025-08-25 18:20:46 -0700

    Update apartment_floor_plan.js

--- assets/models/map/apartment_floor_plan.js
+++ assets/models/map/apartment_floor_plan.js
@@ -1,12 +1,13 @@
-(function () {
+// assets/models/map/apartment_floor_plan.js
+(function(){
   window.MAP_DEF = {
     title: "Apartment",
     file: "apartment_floor_plan.glb",
     scale: 1.0,
     rotationY: 0,
     offset: { x: 0, y: 0, z: 0 },
-    // Set this to a good front-door / van zone for the apartment file:
-    vanZone: { center: { x: 0.0, y: 1.8, z: 0.0 }, radius: 11 },
+    vanZone: { center: { x: 0, y: 1.8, z: 0 }, radius: 4 },
+    exterior: [ {x:-30,z:-30}, {x:30,z:-30}, {x:30,z:30}, {x:-30,z:30} ],
     rooms: []
   };
 })();
