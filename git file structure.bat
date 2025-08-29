db998dafac2f04dc0dacbc04f595722ea85ee3ba
sharkvelocity <71565691+sharkvelocity@users.noreply.github.com>
2025-08-29 03:06:03 -0700

    getting ready to push

--- git file structure.bat
+++ git file structure.bat
@@ -3,7 +3,5 @@ git add .
 REM === Commit the restructuring ===
 git commit -m "getting ready to push"
 
-echo Done. Push with: git push origin main
-pause
-git push origin main:3d
+echo Done. dont forget to push origin on git
 pause
