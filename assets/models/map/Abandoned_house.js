9ab9a82197a5d7b43d1d43a05d908e81c4769848
sharkvelocity <71565691+sharkvelocity@users.noreply.github.com>
2025-09-18 08:11:03 -0700

    Update Abandoned_house.js

--- assets/models/map/Abandoned_house.js
+++ assets/models/map/Abandoned_house.js
@@ -4,7 +4,7 @@
     file: "Abandoned_House.glb",
     scale: 1.0,
     rotationY: 0,
-    offset: { x: 0, y: 0, z: 0 },
+    offset: { x: 0, y: -5, z: 0 },
 
     // ✅ updated vanZone spawn
     vanZone: { center: { x: -2.12, y: 0, z: -9.96 }, radius: 8 },
